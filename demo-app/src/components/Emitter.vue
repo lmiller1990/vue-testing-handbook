68fbdfb7ed4fbc2fcb1cf0636dcb04287c910db3
lmiller1990 <lachlan.miller.1990@outlook.com>
2018-07-06 23:43:06 +0900

    Add japanese guide about emitting events:

---
+++ demo-app/src/components/Emitter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Emitter",
+
+    methods: { 
+      emitEvent() {
+        this.$emit("myEvent", "name", "password")
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
