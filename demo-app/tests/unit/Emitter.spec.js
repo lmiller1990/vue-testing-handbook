73446c80284beeb64730f0e43b821339e0908667
Lachlan <lachlan.miller.1990@outlook.com>
2018-07-07 14:05:49 +0900

    イベントのテストのガイドを追加します (#14)
    * Add japanese guide about emitting events:
    
    * Change test desc to japaense
    
    * Update test description output
    
    * Update title
    
    * Update titles
    
    * Add link to source code
    
    * Update guide with feedback

---
+++ demo-app/tests/unit/Emitter.spec.js
@@ -0,0 +1,21 @@
+import Emitter from "@/components/Emitter.vue"
+import { shallowMount } from "@vue/test-utils"
+
+describe("Emitter", () => {
+  it("emits an event with two arguments", () => {
+    const wrapper = shallowMount(Emitter)
+
+    wrapper.vm.emitEvent()
+
+    expect(wrapper.emitted().myEvent[0]).toEqual(["name", "password"])
+  })
+
+  it("emits an event without mounting the component", () => {
+    const events = {}
+    const $emit = (event, ...args) => { events[event] = [...args] }
+
+    Emitter.methods.emitEvent.call({ $emit })
+
+    expect(events.myEvent).toEqual(["name", "password"])
+  })
+})
