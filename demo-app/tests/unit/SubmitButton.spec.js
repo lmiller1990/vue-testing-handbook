cd1c23fc6a1da7bf2a3b58ab81b075ca3bf4f56d
Yuki <yuki.dees39th@gmail.com>
2018-07-11 00:25:50 +0900

    update filename and fix typo

--- demo-app/tests/unit/SubmitButton.spec.js
+++ demo-app/tests/unit/SubmitButton.spec.js
@@ -1,18 +1,16 @@
 import { shallowMount } from '@vue/test-utils'
-import ColorButton from '@/components/ColorButton.vue'
+import SubmitButton from '@/components/SubmitButton.vue'
 
-describe('Greeting.vue', () => {
+describe('SubmitButton.vue', () => {
   describe("リファクタリング前", () => {
     it('権限がない状態のメッセージを表示する', () => {
       const msg = "送信する"
-      const wrapper = shallowMount(ColorButton,{
+      const wrapper = shallowMount(SubmitButton,{
         propsData: {
           msg: msg
         }
       })
 
-      console.log(wrapper.html())
-  
       expect(wrapper.find("span").text()).toBe("権限がありません")
       expect(wrapper.find("button").text()).toBe("送信する")
     })
@@ -20,15 +18,13 @@ describe('Greeting.vue', () => {
     it('権限がある状態のメッセージを表示する', () => {
       const msg = "送信する"
       const isAdmin = true
-      const wrapper = shallowMount(ColorButton,{
+      const wrapper = shallowMount(SubmitButton,{
         propsData: {
           msg,
           isAdmin
         }
       })
 
-      console.log(wrapper.html())
-  
       expect(wrapper.find("span").text()).toBe("管理者権限を実行する")
       expect(wrapper.find("button").text()).toBe("送信する")
     })
@@ -39,7 +35,7 @@ describe('Greeting.vue', () => {
   describe("リファクタリング後", () => {
     const msg = "送信する"
     const factory = (propsData) => {
-      return shallowMount(ColorButton, {
+      return shallowMount(SubmitButton, {
         propsData: {
           msg,
           ...propsData
