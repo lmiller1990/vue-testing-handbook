bd8a058bd6f18c966789247390d413293c8509d1
lmiller1990 <lachlan.miller.1990@outlook.com>
2018-07-15 18:55:27 +0900

    Add mocks guide

---
+++ demo-app/tests/unit/Bilingual.spec.js
@@ -0,0 +1,8 @@
+import { shallowMount } from "@vue/test-utils"
+import Bilingual from "@/components/Bilingual.vue"
+
+describe("Bilingual", () => {
+  it("renders successfully", () => {
+    const wrapper = shallowMount(Bilingual)
+  })
+})
