d00b672254c7158296116edb2e73dffd37bf4f56
lmiller1990 <lachlan.miller.1990@outlook.com>
2018-07-15 18:58:08 +0900

    Lint

--- demo-app/src/components/Bilingual.vue
+++ demo-app/src/components/Bilingual.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-  export default {
-    name: "Bilingual"
-  }
+export default {
+  name: "Bilingual"
+}
 </script>
--- demo-app/tests/unit/Bilingual.spec.js
+++ demo-app/tests/unit/Bilingual.spec.js
@@ -4,5 +4,7 @@ import Bilingual from "@/components/Bilingual.vue"
 describe("Bilingual", () => {
   it("renders successfully", () => {
     const wrapper = shallowMount(Bilingual)
+
+    expect(wrapper.find(".hello").text()).not.toBe("")
   })
 })
