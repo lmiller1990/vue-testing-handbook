d7ffd0fdf5d39356d4625c77e924b7853f523a29
Lachlan <lachlan.miller.1990@outlook.com>
2018-07-18 01:47:12 +0900

    Write about `find` and `findAll` (#33)
    * Start finding guide
    
    * Finish guide
    
    * Add component
    
    * Remove unused var
    
    * Proofread

---
+++ demo-app/tests/unit/Parent.spec.js
@@ -0,0 +1,46 @@
+import { shallowMount } from "@vue/test-utils"
+import Parent from "@/components/Parent.vue"
+import ParentWithManyChildren from "@/components/ParentWithManyChildren.vue"
+import Child from "@/components/Child.vue"
+
+describe("Parent", () => {
+  it("does not render a span", () => {
+    const wrapper = shallowMount(Parent)
+
+    expect(wrapper.find("span").isVisible()).toBe(false)
+  })
+
+  it("does render a span", () => {
+    const wrapper = shallowMount(Parent, {
+      data() {
+        return { showSpan: true }
+      }
+    })
+
+    expect(wrapper.find("span").isVisible()).toBe(true)
+  })
+
+  it("does not render a Child component", () => {
+    const wrapper = shallowMount(Parent)
+
+    expect(wrapper.find(Child).exists()).toBe(false)
+  })
+
+  it("renders a Child component", () => {
+    const wrapper = shallowMount(Parent, {
+      data() {
+        return { showChild: true }
+      }
+    })
+
+    expect(wrapper.find({ name: "Child" }).exists()).toBe(true)
+  })
+})
+
+describe("ParentWithManyChildren", () => {
+  it("renders many children", () => {
+    const wrapper = shallowMount(ParentWithManyChildren)
+
+    expect(wrapper.findAll(Child).length).toBe(3)
+  })
+})
