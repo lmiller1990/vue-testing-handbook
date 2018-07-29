fc7b60c409bbbe6948280d8d70d8dcb6b951968c
lmiller1990 <lachlan.miller.1990@outlook.com>
2018-07-29 22:26:02 +0900

    Add guide about getters

--- demo-app/src/main.js
+++ demo-app/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from "./router.js"
+import store from "./store/index.js"
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
