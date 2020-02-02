5cc210d275207287224147d610553fe49431bc5a
Lachlan <lachlan.miller.1990@outlook.com>
2020-02-02 23:19:03 +1000

    Revert "Korean translation"

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -5,37 +5,17 @@ module.exports = {
     [
       '@vuepress/google-analytics',
       {
-        ga: 'UA-122389064-1',
-      },
-    ],
+        'ga': 'UA-122389064-1'
+      }
+    ]
   ],
   head: [
-    [
-      'link',
-      {
-        rel: 'icon',
-        href:
-          'https://lmiller1990.github.io/vue-testing-handbook/img/favicon.png',
-      },
-    ],
-    ['meta', { property: 'og:title', content: 'Vue Testing Handbook' }],
-    ['meta', { property: 'og:description', content: 'Vue testing handbook' }],
-    ['meta', { property: 'og:type', content: 'website' }],
-    [
-      'meta',
-      {
-        property: 'og:url',
-        content: 'https://lmiller1990.github.io/vue-testing-handbook/',
-      },
-    ],
-    [
-      'meta',
-      {
-        property: 'og:image',
-        content:
-          'https://lmiller1990.github.io/vue-testing-handbook/img/og.png',
-      },
-    ],
+    ['link', { rel: 'icon', href: 'https://lmiller1990.github.io/vue-testing-handbook/img/favicon.png' }],
+    ['meta', { property:'og:title', content:'Vue Testing Handbook'}],
+    ['meta', { property:'og:description', content:'Vue testing handbook'}],
+    ['meta', { property:'og:type', content:'website'}],
+    ['meta', { property:'og:url', content:'https://lmiller1990.github.io/vue-testing-handbook/'}],
+    ['meta', { property:'og:image', content: 'https://lmiller1990.github.io/vue-testing-handbook/img/og.png' }],
   ],
   locales: {
     '/': {
@@ -52,12 +32,8 @@ module.exports = {
     },
     '/zh-CN/': {
       lang: 'zh-CN',
-      title: 'Vue测试指南',
-    },
-    '/ko/': {
-      lang: 'ko',
-      title: 'Vue 테스팅 핸드북',
-    },
+      title: 'Vue测试指南'
+    }
   },
   themeConfig: {
     repo: 'lmiller1990/vue-testing-handbook',
@@ -78,24 +54,18 @@ module.exports = {
           ['/testing-emitted-events', 'Testing emitted events'],
           ['/mocking-global-objects', 'Mocking global objects'],
           ['/stubbing-components', 'Stubbing components'],
-          [
-            '/finding-elements-and-components',
-            'Finding elements and components',
-          ],
+          ['/finding-elements-and-components', 'Finding elements and components'],
           ['/testing-vuex', 'Testing Vuex'],
           ['/vuex-mutations', 'Vuex - Mutations'],
           ['/vuex-actions', 'Vuex - Actions'],
           ['/vuex-getters', 'Vuex - Getters'],
           ['/vuex-in-components', 'Vuex in components - $state and getters'],
-          [
-            '/vuex-in-components-mutations-and-actions',
-            'Vuex in components - mutations and actions',
-          ],
+          ['/vuex-in-components-mutations-and-actions', 'Vuex in components - mutations and actions'],
           ['/vue-router', 'Vue Router'],
           ['/composition-api', 'Composition API'],
           ['/reducing-boilerplate-in-tests', 'Reducing Boilerplate'],
           ['/jest-mocking-modules', 'Jest - mocking modules'],
-        ],
+        ]
       },
       '/ja/': {
         label: '日本語',
@@ -112,23 +82,17 @@ module.exports = {
           ['/ja/testing-emitted-events', '発生したイベントのテスト'],
           ['/ja/mocking-global-objects', 'グローバルオブジェクトのモック'],
           ['/ja/stubbing-components', 'コンポーネントをスタブする'],
-          [
-            '/ja/finding-elements-and-components',
-            '要素とコンポーネントを検索する',
-          ],
+          ['/ja/finding-elements-and-components', '要素とコンポーネントを検索する'],
           ['/ja/testing-vuex', 'Vuexのテストの紹介'],
           ['/ja/vuex-mutations', 'Vuex - ミューテーション'],
           ['/ja/vuex-actions', 'Vuex - アクション'],
           ['/ja/vuex-getters', 'Vuex - ゲッター'],
           ['/ja/vuex-in-components', 'コンポーネントの中でVuexのテスト'],
-          [
-            '/ja/vuex-in-components-mutations-and-actions.md',
-            'Vuex in components - mutations and actions',
-          ],
+          ['/ja/vuex-in-components-mutations-and-actions.md', 'Vuex in components - mutations and actions'],
           ['/ja/vue-router', 'Vueルーター'],
           ['/ja/jest-mocking-modules', 'Jestでモジュールをモック'],
           ['/ja/composition-api', 'Composition API'],
-        ],
+        ]
       },
       '/ru/': {
         label: 'Русский',
@@ -141,34 +105,22 @@ module.exports = {
           ['/ru/rendering-a-component', 'Отрисовка компонентов'],
           ['/ru/components-with-props', 'Тестирование входных параметров'],
           ['/ru/computed-properties', 'Вычисляемые свойства'],
-          [
-            '/ru/simulating-user-input',
-            'Симулирование пользовательского ввода',
-          ],
-          [
-            '/ru/testing-emitted-events',
-            'Тестирование пользовательских событий',
-          ],
+          ['/ru/simulating-user-input', 'Симулирование пользовательского ввода'],
+          ['/ru/testing-emitted-events', 'Тестирование пользовательских событий'],
           ['/ru/mocking-global-objects', 'Мокаем глобальные объекты'],
           ['/ru/stubbing-components', 'Заглушки для компонентов'],
-          [
-            '/ru/finding-elements-and-components',
-            'Поиск элементов и компонентов',
-          ],
+          ['/ru/finding-elements-and-components', 'Поиск элементов и компонентов'],
           ['/ru/testing-vuex', 'Тестирование Vuex'],
           ['/ru/vuex-mutations', 'Vuex - Мутации'],
           ['/ru/vuex-actions', 'Vuex - Действия'],
           ['/ru/vuex-getters', 'Vuex - Геттеры'],
           ['/ru/vuex-in-components', 'Vuex в компонентах - $state и геттеры'],
-          [
-            '/ru/vuex-in-components-mutations-and-actions',
-            'Vuex в компонентах - мутации и действия',
-          ],
+          ['/ru/vuex-in-components-mutations-and-actions', 'Vuex в компонентах - мутации и действия'],
           ['/ru/vue-router', 'Vue Router'],
           ['/ru/composition-api', 'Composition API'],
           ['/ru/reducing-boilerplate-in-tests', 'Уменьшаем шаблонный код'],
           ['/ru/jest-mocking-modules', 'Jest - мокаем модули'],
-        ],
+        ]
       },
       '/zh-CN/': {
         label: '简体中文',
@@ -191,52 +143,18 @@ module.exports = {
           ['/zh-CN/vuex-actions', 'Vuex - Actions'],
           ['/zh-CN/vuex-getters', 'Vuex - Getters'],
           ['/zh-CN/vuex-in-components', '组件中的 Vuex - $state 和 getters'],
-          [
-            '/zh-CN/vuex-in-components-mutations-and-actions',
-            '组件中的 Vuex - mutations 和 actions',
-          ],
+          ['/zh-CN/vuex-in-components-mutations-and-actions', '组件中的 Vuex - mutations 和 actions'],
           ['/zh-CN/vue-router', 'Vue Router'],
           ['/zh-CN/composition-api', 'Composition API'],
           ['/zh-CN/jest-mocking-modules', 'Jest - mock 模块'],
-        ],
+        ]
       },
-      '/ko/': {
-        label: '한국어',
-        selectText: '다른 언어로 보기',
-        lastUpdated: '마지막 업데이트',
-        editLinkText: 'Github에서 이 페이지 수정하기',
-        sidebar: [
-          ['/ko/', '환영합니다'],
-          ['/ko/setting-up-for-tdd', 'TDD를 위한 설정하기'],
-          ['/ko/rendering-a-component', '컴포넌트 렌더하기'],
-          ['/ko/components-with-props', 'props 테스트하기'],
-          ['/ko/computed-properties', 'computed 프로퍼티'],
-          ['/ko/simulating-user-input', '사용자 입력 시연하기'],
-          ['/ko/testing-emitted-events', 'emitted 이벤트 테스트하기'],
-          ['/ko/mocking-global-objects', '전역 객체 모킹하기'],
-          ['/ko/stubbing-components', '컴포넌트 스터빙'],
-          ['/ko/finding-elements-and-components', '엘리먼트와 컴포넌트 찾기'],
-          ['/ko/testing-vuex', 'Vuex 테스트하기'],
-          ['/ko/vuex-mutations', 'Vuex - 뮤테이션'],
-          ['/ko/vuex-actions', 'Vuex - 액션'],
-          ['/ko/vuex-getters', 'Vuex - 게터'],
-          ['/ko/vuex-in-components', '컴포넌트 내에 있는 Vuex - $state와 게터'],
-          [
-            '/ko/vuex-in-components-mutations-and-actions',
-            '컴포넌트 내에 있는 Vuex - 뮤테이션과 액션',
-          ],
-          ['/ko/vue-router', 'Vue 라우터'],
-          ['/ko/composition-api', '컴포지션 API'],
-          ['/ko/reducing-boilerplate-in-tests', '보일러플레이트 줄이기'],
-          ['/ko/jest-mocking-modules', 'Jest - 모듈 모킹하기'],
-        ],
-      },
-    },
+    }
   },
   markdown: {
     config: md => {
       // use more markdown-it plugins!
       md.use(require('markdown-it-include'))
-    },
-  },
-}
+    }
+  }
+};
