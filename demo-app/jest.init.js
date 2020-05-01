38152f5d3b2637e775761fcb72a8266d2fca6a1d
Karla Hernández <ing.karla.hernandez@gmail.com>
2020-04-30 21:38:03 -0500

    Adding support to nested items
    This change will provide support to nested items in translation files by splitting the passed message and then search the correct translation.

--- demo-app/jest.init.js
+++ demo-app/jest.init.js
@@ -3,4 +3,11 @@ import translations from "./src/translations.js"
 
 const locale = "en"
 
-config.mocks["$t"] = (msg) => translations[locale][msg]
+config.mocks.$t = (msg) => {
+  const subobjects = msg.split('.');
+  let translationMockMessage = translations[locale][msg];
+  subobjects.forEach((element) => {
+    translationMockMessage = translationMockMessage[element];
+  });
+  return translationMockMessage;
+};
