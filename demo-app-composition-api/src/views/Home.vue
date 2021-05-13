94e0521128d4c380c3dbd9cf5c5dba9549fb81fc
Hilton Meyer <development@hiltonmeyer.com>
2021-05-13 11:11:15 +0300

    feat(home): Add button component

--- demo-app-composition-api/src/views/Home.vue
+++ demo-app-composition-api/src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="home">
     <Greeting />
+    <SubmitButton msg="Login" />
+    <SubmitButton msg="Logout" isAdmin="true" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Greeting from '../components/Greeting.vue';
+import SubmitButton from '../components/SubmitButton.vue';
 
 export default {
   name: 'Home',
   components: {
     Greeting,
+    SubmitButton,
   },
 };
 </script>
