29b4b4cf7ee2392c09f573b1dd32e86dc2ede442
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-06-25 18:36:07 +1000

    composition api course info

--- src/.vuepress/theme/components/Sidebar.vue
+++ src/.vuepress/theme/components/Sidebar.vue
@@ -3,27 +3,27 @@
     <!--
       <CarbonAds />
     -->
-    <a href="https://lachlan-miller.me/design-patterns-for-vuejs/" target="_blank">
+    <a :href="href" target="_blank">
       <img
         id="ad"
         style="border: 1px solid rgba(0,0,0,0.5);"
-        src="https://raw.githubusercontent.com/lmiller1990/vue-testing-handbook/master/src/.vuepress/public/mini.png"
+        src="https://raw.githubusercontent.com/lmiller1990/vue-testing-handbook/master/src/.vuepress/public/composition.png"
         alt='banner'
       />
     </a>
     <br />
     <div class="info">
       <small>
-      Get $5 off my new book,
-      <a href="https://lachlan-miller.me/design-patterns-for-vuejs" target="_blank"><em>Design</em></a>
-      <a href="https://lachlan-miller.me/design-patterns-for-vuejs" target="_blank"><em>Patterns</em></a>
-      <a href="https://lachlan-miller.me/design-patterns-for-vuejs" target="_blank"><em>for</em></a>
-      <a href="https://lachlan-miller.me/design-patterns-for-vuejs" target="_blank"><em>Vue.js</em></a>
-      <a href="https://lachlan-miller.me/design-patterns-for-vuejs" target="_blank"><em>a test driven</em></a>
-      <a href="https://lachlan-miller.me/design-patterns-for-vuejs" target="_blank"><em>approach to</em></a>
-      <a href="https://lachlan-miller.me/design-patterns-for-vuejs" target="_blank"><em>maintainable</em></a>
-      <a href="https://lachlan-miller.me/design-patterns-for-vuejs" target="_blank"><em>applications</em>,</a>
-      with <b>DESIGN_5_OFF</b> at the discount. 🎉
+      Get $10 off my new course,
+      <a 
+        target="_blank" 
+        v-for="word in ['Vue.js:', 'The', 'Composition', 'API,', 'TypeScript', 'and', 'Testing']" 
+        :key="word"
+        :href="href" 
+      >
+        <em>{{ word }}&nbsp;</em>
+      </a>with discount code <b>HANDBOOK</b> at the checkout. 🎉 . 
+      It's on both <a href="https://gumroad.com/l/vuejs-composition-api">Gumroad</a> and <a href="https://www.udemy.com/course/vuejs-3-the-composition-api/?referralCode=0C911BD2BD6C57007F36">Udemy</a>. Preview <a href="https://vimeo.com/showcase/8603383">it here.</a>
       </small>
     </div>
 
@@ -83,6 +83,12 @@ export default {
   components: { SidebarLinks, NavLinks, CarbonAds, VueSchool },
 
   props: ['items'],
+
+  data() {
+    return {
+      href: 'https://gumroad.com/l/vuejs-composition-api'
+    }
+  },
 
   methods: {
     track() {
